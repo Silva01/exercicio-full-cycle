e53793b705830320afc478e8a5fc1aa9ea6830ee
silva01 <silva20102@gmail.com>
2023-01-02 19:49:35 -0300

    Ajusta entidade de produto

--- src/entity/product.ts
+++ src/entity/product.ts
@@ -45,9 +45,11 @@ export default class Product {
 
     changeName(name: string): void {
         this._name = name;
+        this.validate();
     }
 
     changePrice(price: number): void {
         this._price = price;
+        this.validate();
     }
 }
