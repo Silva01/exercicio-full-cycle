4438f2c425e53f4d111cef72ee910bce51591932
silva01 <silva20102@gmail.com>
2023-01-06 20:48:37 -0300

    retirado espaço em branco

--- src/domain/event/product/product-created-event.ts
+++ src/domain/event/product/product-created-event.ts
@@ -6,6 +6,6 @@ export default class ProductCreatedEvent implements EventInterface {
 
     constructor(eventData: any) {
         this.dateTimeOccurred = new Date();
-        this.eventData = eventData;
+        this.eventData = eventData;        
     }
 }
