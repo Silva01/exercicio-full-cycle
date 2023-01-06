29d37a3cd5a9bdcf4f9ff6908ce80464797073d0
silva01 <silva20102@gmail.com>
2023-01-05 21:47:31 -0300

    adicionado event dispatcher

---
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -0,0 +1,31 @@
+import EventDispatcherInterface from "./event-dispatcher.interface";
+import EventHandlerInterface from "./event.handler.interface";
+import EventInterface from "./event.interface";
+
+export default class EventDispatcher implements EventDispatcherInterface {
+    private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
+
+    public notify(event: EventInterface): void {
+        
+    }
+
+    public register(eventName: string, eventHandler: EventHandlerInterface): void {
+        if (!this.eventHandlers[eventName]) {
+            this.eventHandlers[eventName] = [];
+        }
+
+        this.eventHandlers[eventName].push(eventHandler);
+    }
+
+    public unregister(eventName: string, eventHandler: EventHandlerInterface): void {
+        
+    }
+
+    public unregisterAll(): void {
+        this.eventHandlers = {};
+    }
+
+    public getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
+        return this.eventHandlers;
+    }
+}
