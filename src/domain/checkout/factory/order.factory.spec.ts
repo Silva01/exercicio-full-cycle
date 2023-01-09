70005889a6f5bde1963f0b705ca6a71d2b7159a8
silva01 <silva20102@gmail.com>
2023-01-09 20:29:00 -0300

    Adicionado testes unitarios de order factory

---
+++ src/domain/checkout/factory/order.factory.spec.ts
@@ -0,0 +1,28 @@
+import { UUID } from "sequelize";
+import { v4 as uuid } from "uuid";
+import OrderFactory from "./order.factory";
+
+describe("Order factory unit tests", () => {
+    it("should create an order", () => {
+        const orderProps = {
+            id: uuid(),
+            customerId: uuid(),
+            items: [
+                {
+                    id: uuid(),
+                    productId: uuid(),
+                    quantity: 1,
+                    price: 10.00,
+                    name: "Product 1"
+                }
+            ]
+        }
+
+        const order = OrderFactory.create(orderProps);
+
+        expect(order.id).toEqual(orderProps.id);
+        expect(order.customerId).toEqual(orderProps.customerId);
+        expect(order.items.length).toBe(1);
+    });
+ 
+});
